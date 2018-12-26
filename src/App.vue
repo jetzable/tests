cbd69842aa0f9a3f0a88493de8352c58b3570379
jetzable <gjetzable@gmail.com>
2018-12-26 17:27:06 -0600

    Adding list of stores

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app"><BranchPicker /></div>
+</template>
+
+<script>
+import BranchPicker from "./components/BranchPicker.vue";
+
+export default {
+  name: "app",
+  components: {
+    BranchPicker
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
